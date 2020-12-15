1a38d743e9964e60fba38aa1e4e245c6a790f9b6
mert <uzeyirmertciflikli@gmail.com>
2020-12-15 13:13:56 +0100

    add todo button

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -16,11 +17,17 @@ export default function App() {
     });
   };
 
+  const submitHandler = text => {
+    setTodos(prevTodos => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+
+  };
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/*todo form*/}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
